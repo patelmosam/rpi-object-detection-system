e107c0dbba78fc03e4f2027ea7879394340ae1d3
Mosam Patel <patelmosam280@gmail.com>
2021-04-24 11:12:39 +0530

    changed no_det to num_det in databese table

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def Init_db(db_path):
     conn = sqlite3.connect(db_path)
     print("Opened database successfully")
 
-    conn.execute('CREATE TABLE Detections (image_id TEXT, bbox TEXT, classes TEXT, scores TEXT, no_det TEXT)')
+    conn.execute('CREATE TABLE Detections (image_id TEXT, bbox TEXT, classes TEXT, scores TEXT, num_det TEXT)')
     print("Table created successfully")
     conn.close()
 
@@ -13,7 +13,7 @@ def insert_data(db_path, data):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
 
-    c.execute("INSERT INTO Detections(image_id, bbox, classes, scores, no_det) VALUES (?,?,?,?,?)", data)
+    c.execute("INSERT INTO Detections(image_id, bbox, classes, scores, num_det) VALUES (?,?,?,?,?)", data)
 
     conn.commit()
 
@@ -30,11 +30,11 @@ def get_all_data(db_path):
     conn.close()
     return row
 
-def get_data_id(db_path, img_id):
+def get_data_id(db_path, image_id):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
 
-    c.execute("SELECT * FROM Detections WHERE image_id=?", (img_id,))
+    c.execute("SELECT * FROM Detections WHERE image_id=?", (image_id,))
     row = c.fetchall()
     conn.close()
     return row
@@ -51,10 +51,3 @@ def delete_data(db_path, image_id):
 if __name__ == '__main__':
     DB_PATH = "./database.db"
     # Init_db(DB_PATH)
-    # conn = sqlite3.connect('database.db')
-
-    #insert_data(conn, (0, [12,23,45,67], [1], [0.99], 1))
-
-    # get_all_data(conn)
-
-    # get_data_id(conn, "2021-04-07-21-46-22")
